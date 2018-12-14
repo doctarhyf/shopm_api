27c92b5a69fd878b5b4c6a638a3b85ead6ab429d
doctarhyf <drrhyf@gmail.com>
2018-12-14 05:50:01 +0200

    qr code

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 
-for($i = 1; $i < 21; $i++){
+include("phpqrcode/qrlib.php");
 
-	echo '&ltitem&gt' . $i . '&lt/item&gt<br/>';
-}
+
+QRcode::png("ip_192.168.1.2", "QRTEST.jpg", QR_ECLEVEL_L, 10);
 
 ?>
