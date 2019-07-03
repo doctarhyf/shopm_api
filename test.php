23baed636a68ce70b44c641d7884207f150e5599
doctarhyf <drrhyf@gmail.com>
2019-07-03 11:56:00 +0200

    added new col [tot vendu] and million comma separator to PDF report

--- test.php
+++ test.php
@@ -1,9 +1,15 @@
 <?php
 
 
-//$fi = new FilesystemIterator(__DIR__, FilesystemIterator::SKIP_DOTS);
-$fi = new FilesystemIterator('rapports/', FilesystemIterator::SKIP_DOTS);
-printf("There were %d Files", iterator_count($fi));
+
+function addCommasDigitSeparator(&$num){
+	
+	if(strlen($num) > 3){
+
+		str_sp	
+	
+	}	
+}
 
 
 
